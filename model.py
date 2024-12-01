91838bfdfeda8f3d511d1916682c57095a30962b
Kawai1Ace <1065699477@qq.com>
2024-12-02 02:03:02 +0800

    [241202] commit2

--- model.py
+++ model.py
@@ -23,4 +23,24 @@ class ModelArgs:
     max_seq_len: int = 2048
     dropout: float=0.0
 
+class RMSNorm(torch.nn.Module):
+    def __init__(self, dim:int, eps:float):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
 
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float()).type_as(x)
+        return output * self.weight
+
+# 频率缩放因子？
+def precompute_freqs_cis(dim: int, end:int, theta: float=10000.0):
+    freqs = 1.0/(theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
+    t = torch.arange(end, device=freqs.device)
+    freqs = torch.outer(t, freqs).float()
+    freqs_cos = torch.cos(freqs)
+    freqs_sin = torch.sin(freqs)
+    return freqs_cos, freqs_sin
